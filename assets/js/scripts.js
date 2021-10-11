45643923b84fee1ae246410dc61d955f8633a65f
bajraroshan <bajraroshan@gmail.com>
2021-10-11 15:32:02 +1030

    Comments on CSS, JS and HTML added and Styling Changes were done

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,61 +1,72 @@
 //set moment to a variable
 var now = moment();
 
-var containerEl = $(".container").addClass("pt-5");
+var containerEl = $(".container").addClass("py-5");
 
 // gets data for the header date
 function getHeaderDate() {
   //create a variable for the current date
   var currentDate = now.format("dddd, MMMM Do");
-  // Set date
+  // Set date to Current day ID
   $("#currentDay").text(currentDate);
 }
 getHeaderDate();
 
 $(document).ready(function () {
-    hourArr = $(".hour").toArray();
-    for (i = 0; i < hourArr.length; i++) {
-      $(hourArr[i])
-        .siblings("textarea")
-        .text(localStorage.getItem($(hourArr[i]).attr("data-time")));
-    }
-  });
+  // Loop to display tasks from local storage
+  hourArr = $(".hour").toArray();
+  for (i = 0; i < hourArr.length; i++) {
+    $(hourArr[i])
+      .siblings("textarea")
+      .text(localStorage.getItem($(hourArr[i]).attr("data-time")));
+  }
+});
 
 for (i = 0; i < 9; i++) {
-  var rowBlock = $("<div>").addClass("row time-block");
+  // variable rowContainer defined with time-block and row class
+  var rowContainer = $("<div>").addClass("row time-block");
+  // variable timeblock defined with hour and col-md-2 class
   var timeBlock = $("<div>")
     .addClass("col-md-2 hour")
-    .text(moment("9:00 AM", "hh:mm A").add(i, "hours").format("hA"));
+    .text(moment("9:00 AM", "hh:mm A").add(i, "hours").format("LT"));
   timeBlock.attr(
     "data-time",
     moment("9:00 AM", "hh:mm A").add(i, "hours").format("hA")
   );
-  var taskBlock = $("<textarea>").addClass("col-md-9");
+  //   variable taskArea defined with col-md-9 class
+  var taskArea = $("<textarea>").addClass("col-md-9");
+  //   variable saveButton defined with col-md-1 and saveBtn class
   var saveButton = $("<button>")
     .addClass("col-md-1 saveBtn")
     .html('<i class="fas fa-save"></i>');
 
-  containerEl.append(rowBlock);
-  $(rowBlock).append(timeBlock);
-  $(timeBlock).after(taskBlock);
-  $(taskBlock).after(saveButton);
+  // append rowContainer in containerEl
+  containerEl.append(rowContainer);
+  //   append timeBlock in rowContainer
+  $(rowContainer).append(timeBlock);
+  //   insert taskArea after timeBlock is displayed
+  $(timeBlock).after(taskArea);
+  //   insert saveButton after taskArea is displayed
+  $(taskArea).after(saveButton);
 
+  //   Condition to determine the time is past, present or future
   if (now.isSame(moment("9:00 AM", "hh:mm A").add(i, "hours"), "hour")) {
-    $(taskBlock).addClass("present");
+    $(taskArea).addClass("present");
   } else if (
     now.isBefore(moment("9:00 AM", "hh:mm A").add(i, "hours"), "hour")
   ) {
-    $(taskBlock).addClass("future");
+    $(taskArea).addClass("future");
   } else if (
     now.isAfter(moment("9:00 AM", "hh:mm A").add(i, "hours"), "hour")
   ) {
-    $(taskBlock).addClass("past");
+    $(taskArea).addClass("past");
   }
 }
 
+// click event on saveBtn to store the data-time attribute and textarea value to localstorage
 $(".saveBtn").on("click", function () {
-    localStorage.setItem(
-      $(this).siblings("div.hour").attr("data-time"),
-      $(this).siblings("textarea").val()
-    );
-  });
+  localStorage.setItem(
+    $(this).siblings("div.hour").attr("data-time"),
+    $(this).siblings("textarea").val()
+  );
+});
